4796a9151da2b3540c4af1e18bbf4076a7201df0
Bruno Rijsman <brunorijsman@hotmail.com>
2025-10-13 13:39:27 +0200

    Solve puzzle

--- day_12_b/src/main.rs
+++ day_12_b/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs::read_to_string, result};
+use memoize::memoize;
+use std::fs::read_to_string;
 
 fn main() {
     let mut total = 0;
@@ -11,7 +12,8 @@ fn main() {
 fn nr_arrangements_for_record(record: &str) -> u64 {
     let (short_symbols, counts) = record.split_once(' ').unwrap();
     let short_symbols = String::from(short_symbols);
-    // TODO: Add a dot (explain why)
+    // Quintuple the symbols as requested in the puzzle.
+    // Add a trailing . to ensure the last group of #'s is terminated.
     let symbols = format!(
         "{}?{}?{}?{}?{}.",
         short_symbols, short_symbols, short_symbols, short_symbols, short_symbols
@@ -28,36 +30,27 @@ fn nr_arrangements_for_record(record: &str) -> u64 {
         &short_counts[..],
     ]
     .concat();
-    println!(
-        "Counting arrangements for symbols: {:?} counts: {:?}",
-        symbols, counts
-    );
-    let nr_arrangements = nr_arrangements(0, &symbols, &counts, false);
-    println!("Found {} arrangements", nr_arrangements);
+    let nr_arrangements = nr_arrangements(symbols, counts, false);
     nr_arrangements
 }
 
-fn nr_arrangements(level: usize, symbols: &String, counts: &[u32], in_hashes: bool) -> u64 {
-    let is = indent_str(level);
-    println!(
-        "{is}* Nr assignments for symbols: {:?} counts: {:?} in_hashes: {}",
-        symbols, counts, in_hashes
-    );
+#[memoize] // Memoization is essential for performance
+fn nr_arrangements(symbols: String, counts: Vec<u32>, in_hashes: bool) -> u64 {
     if symbols.is_empty() {
         return if counts.is_empty() {
-            // println!("{is}- Symbols is empty and counts is also empty => 1 match");
+            // Symbols is empty and counts is also empty => 1 match
             1
         } else {
-            // println!("{is}- Symbols is empty but counts is not empty => 0 match");
+            // Symbols is empty but counts is not empty => 0 match
             0
         };
     }
     if counts.is_empty() {
         return if symbols.chars().all(|c| c != '#') {
-            // println!("{is}- Counts is empty and remaining symbols are . or ? => 1 match");
+            // Counts is empty and remaining symbols are . or ? => 1 match
             1
         } else {
-            // println!("{is}- Counts is empty and remaining symbols have # => 0 match");
+            // Counts is empty and remaining symbols have # => 0 match
             0
         };
     }
@@ -65,71 +58,52 @@ fn nr_arrangements(level: usize, symbols: &String, counts: &[u32], in_hashes: bo
     let rest_symbols = String::from(&symbols[1..]);
     let first_count = counts[0];
     let result = if first_symbol == '#' {
-        // println!("{is}- First symbol is #");
+        // First symbol is #
         if in_hashes {
-            // println!("{is}- Continuing in group of hashes");
+            // Continuing in group of #'s
             if first_count == 0 {
-                // println!("{is}- Did not expect another hash -> 0 match");
+                // Did not expect another # => 0 match
                 0
             } else {
-                // println!("{is}- Consume a hash");
+                // Consume the #
                 let mut new_counts = counts.to_vec();
                 new_counts[0] -= 1;
-                nr_arrangements(level + 1, &rest_symbols, &new_counts, true)
+                nr_arrangements(rest_symbols, new_counts, true)
             }
         } else {
             assert!(first_count > 0);
-            // println!("{is}- Starting new group of hashes, consume first hash");
+            // Starting new group of #'s, consume first #
             let mut new_counts = counts.to_vec();
             new_counts[0] -= 1;
-            nr_arrangements(level + 1, &rest_symbols, &new_counts, true)
+            nr_arrangements(rest_symbols, new_counts, true)
         }
     } else if first_symbol == '.' {
-        // println!("{is}- First symbol is .");
+        // First symbol is .
         if in_hashes {
-            // println!("{is}- Ending group of hashes");
+            // Ending group of #'s
             if first_count == 0 {
-                // println!("{is}- Consumed expected number of hashes");
-                let new_counts = &counts[1..];
-                nr_arrangements(level + 1, &rest_symbols, new_counts, false)
+                // Consumed expected number of #'s
+                let new_counts = counts[1..].to_vec();
+                nr_arrangements(rest_symbols, new_counts, false)
             } else {
-                // println!("{is}- Expected more hashes -> 0 match");
+                // Expected more #'s => 0 match
                 0
             }
         } else {
-            // println!("{is}- Continuing in dots");
-            nr_arrangements(level + 1, &rest_symbols, counts, false)
+            // Continuing in .'s
+            nr_arrangements(rest_symbols, counts, false)
         }
     } else if first_symbol == '?' {
+        // First symbol is ?, try both # and .
         let rest_symbols = String::from(&symbols[1..]);
         let dash_symbol = format!("#{}", &rest_symbols);
         let dot_symbol = format!(".{}", &rest_symbols);
-        let hash_arrangements = nr_arrangements(level + 1, &dash_symbol, counts, in_hashes);
-        let dot_arrangements = nr_arrangements(level + 1, &dot_symbol, counts, in_hashes);
+        let hash_arrangements = nr_arrangements(dash_symbol, counts.clone(), in_hashes);
+        let dot_arrangements = nr_arrangements(dot_symbol, counts.clone(), in_hashes);
         let total_arrangements = hash_arrangements + dot_arrangements;
-        // println!(
-        //     "{is}- First symbol is ?, sum arrangements for # and . => {} + {} = {}",
-        //     hash_arrangements, dot_arrangements, total_arrangements
-        // );
-        // println!(
-        //     "{is}- Nr assignments for symbols: {:?} and counts: {:?} => {}",
-        //     symbols, counts, total_arrangements
-        // );
         total_arrangements
     } else {
         panic!("Unexpected symbol: {}", first_symbol);
     };
-    // println!(
-    //     "{is}< Nr assignments for symbols: {:?} counts: {:?} in_hashes: {} RESULT: {}",
-    //     symbols, counts, in_hashes, result
-    // );
     result
-}
-
-fn indent_str(level: usize) -> String {
-    let mut s = String::new();
-    for _ in 0..level * 2 {
-        s.push(' ');
-    }
-    s
 }
