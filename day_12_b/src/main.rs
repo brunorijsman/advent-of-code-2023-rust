7f99e08ae5d227da904f330140720ab2ea3180df
Bruno Rijsman <brunorijsman@hotmail.com>
2025-10-13 11:55:59 +0200

    Third attempt, also no good

--- day_12_b/src/main.rs
+++ day_12_b/src/main.rs
@@ -13,11 +13,6 @@ fn nr_possible_arrangements(record: &str) -> u64 {
     let short_symbols = String::from(short_symbols);
     // let symbols = format!("{}{}{}", short_symbols, short_symbols, short_symbols);
     let symbols = format!("{}", short_symbols);
-    let symbol_groups = symbols
-        .split('.')
-        .filter(|s| !s.is_empty())
-        .map(|s| String::from(s))
-        .collect();
     let short_counts: Vec<u32> = counts
         .split(',')
         .map(|s| s.parse::<u32>().unwrap())
@@ -25,74 +20,92 @@ fn nr_possible_arrangements(record: &str) -> u64 {
     // let counts = [&short_counts[..], &short_counts[..], &short_counts[..]].concat();
     let counts = [&short_counts[..]].concat();
     println!(
-        "Counting arrangements for symbol groups: {:?} and counts: {:?}",
-        symbol_groups, counts
+        "Counting arrangements for symbols: {:?} and counts: {:?}",
+        symbols, counts
     );
-    let nr_arrangements = total_nr_assignments(&symbol_groups, &counts);
+    let nr_arrangements = nr_assignments(&symbols, &counts);
     println!("Found {} arrangements", nr_arrangements);
     nr_arrangements
 }
 
-fn total_nr_assignments(symbol_groups: &Vec<String>, counts: &[u32]) -> u64 {
-    if symbol_groups.is_empty() {
-        if counts.is_empty() {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-    let first_symbol_group = symbol_groups[0].clone();
-    let rest_symbol_groups = &symbol_groups[1..].to_vec();
-    let mut total_nr_assignements = 0;
-    for split_pos in 0..counts.len() {
-        let match_counts = counts[0..split_pos].to_vec();
-        let rest_counts = counts[split_pos..].to_vec();
-        let first_nr_assignments = symbol_group_nr_assignments(&first_symbol_group, &match_counts);
-        if first_nr_assignments > 0 {
-            let rest_nr_assignments = total_nr_assignments(rest_symbol_groups, &rest_counts);
-            println!(
-                "first_nr_assignments: {}, rest_nr_assignments: {}",
-                first_nr_assignments, rest_nr_assignments
-            );
-            total_nr_assignements += first_nr_assignments * rest_nr_assignments;
-        }
-    }
-    total_nr_assignements
-}
-
-fn symbol_group_nr_assignments(symbol_group: &String, counts: &[u32]) -> u64 {
+fn nr_assignments(symbols: &String, counts: &[u32]) -> u64 {
     println!(
-        "Assignments for group {:?} counts {:?}",
-        symbol_group, counts
+        "Nr assignments for symbols: {:?} and counts: {:?}",
+        symbols, counts
     );
-    // Empty counts list => only match if symbol group is all '?'
+    if symbols.is_empty() {
+        return if counts.is_empty() {
+            println!(" - Symbols is empty and counts is also empty => 1 match");
+            1
+        } else {
+            println!(" - Symbols is empty but counts is not empty => 0 match");
+            0
+        };
+    }
     if counts.is_empty() {
-        if symbol_group.chars().all(|c| c == '?') {
-            println!(
-                "- Assignments for symbol group: {} and counts: {:?} => ONE",
-                symbol_group, counts
-            );
-            return 1;
+        return if symbols.chars().all(|c| c != '#') {
+            println!(" - Counts is empty and remaining symbols are . or ? => 1 match");
+            1
         } else {
-            println!(
-                "- Assignments for symbol group: {} and counts: {:?} => ZERO",
-                symbol_group, counts
-            );
+            println!(" - Counts is empty and remaining symbols have # => 0 match");
+            0
+        };
+    }
+    let first_symbol = symbols.chars().next().unwrap();
+    if first_symbol == '#' {
+        let mut nr_expected_hashes = counts[0];
+        assert!(nr_expected_hashes > 0);
+        println!(
+            " - First symbol is # and nr expected dashes is {}, consume expected #'s",
+            nr_expected_hashes
+        );
+        if symbols.len() < nr_expected_hashes as usize {
+            println!("   - Not enough symbols left to consume all # => 0 match");
             return 0;
         }
+        let expected_hash_symbols = String::from(&symbols[0..nr_expected_hashes as usize]);
+        if !expected_hash_symbols.chars().all(|c| c == '#') {
+            println!("   - Not enough # symbols at start to consume all # => 0 match");
+            return 0;
+        }
+        let mut rest_symbols = String::from(&symbols[nr_expected_hashes as usize..]);
+        println!(
+            "   - Rest symbols after consuming expected #'s: {:?}",
+            rest_symbols
+        );
+        let first_rest_symbol = rest_symbols.chars().next();
+        if first_rest_symbol == Some('#') {
+            println!("   - Next symbol after consuming # is also # => invalid, 0 match");
+            return 0;
+        }
+        if first_rest_symbol == Some('?') {
+            rest_symbols = String::from(&rest_symbols[1..]);
+            println!("   - Consume terminating ? after contiguous string of #");
+        }
+        let rest_counts = &counts[1..];
+        return nr_assignments(&rest_symbols, &rest_counts);
     }
-    // Consume the number of hashes needed to satisfy the first count in the count vector.
-    // Remember that the symbol group (by definition) consists of only '#' and '?'
-    let need_hashes: u32 = counts[0];
-    if symbol_group.len() < need_hashes as usize {
-        return 0;
+    if first_symbol == '.' {
+        println!(" - First symbol is ., continue with rest symbols and same counts");
+        let rest_symbols = String::from(&symbols[1..]);
+        return nr_assignments(&rest_symbols, counts);
     }
-    let rest_symbol_group = String::from(&symbol_group[need_hashes as usize..]);
-    let rest_counts = &counts[1..];
-    let nr_assignments = symbol_group_nr_assignments(&rest_symbol_group, rest_counts);
-    println!(
-        "- Assignments for symbol group: {} and counts: {:?} => {}",
-        symbol_group, counts, nr_assignments
-    );
-    nr_assignments
+    if first_symbol == '?' {
+        let rest_symbols = String::from(&symbols[1..]);
+        let dash_symbol = format!("#{}", &rest_symbols);
+        let dot_symbol = format!(".{}", &rest_symbols);
+        let hash_arrangements = nr_assignments(&dash_symbol, counts);
+        let dot_arrangements = nr_assignments(&dot_symbol, counts);
+        let total_arrangements = hash_arrangements + dot_arrangements;
+        println!(
+            " - First symbol is ?, sum arrangements for # and . => {} + {} = {}",
+            hash_arrangements, dot_arrangements, total_arrangements
+        );
+        println!(
+            " * Nr assignments for symbols: {:?} and counts: {:?} => {}",
+            symbols, counts, total_arrangements
+        );
+        return total_arrangements;
+    }
+    panic!("Unexpected symbol: {}", first_symbol);
 }
