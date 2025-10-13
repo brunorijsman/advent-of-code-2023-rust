625f1997a23e3e2f91e2e44a3fbeb6338f7dba77
Bruno Rijsman <brunorijsman@hotmail.com>
2025-10-13 13:24:49 +0200

    Minor tweaks

--- day_12_b/src/main.rs
+++ day_12_b/src/main.rs
@@ -3,12 +3,12 @@ use std::{fs::read_to_string, result};
 fn main() {
     let mut total = 0;
     for record in read_to_string("puzzle_input").unwrap().lines() {
-        total += nr_possible_arrangements(record);
+        total += nr_arrangements_for_record(record);
     }
     println!("Total number of possible arrangements: {}", total);
 }
 
-fn nr_possible_arrangements(record: &str) -> u64 {
+fn nr_arrangements_for_record(record: &str) -> u64 {
     let (short_symbols, counts) = record.split_once(' ').unwrap();
     let short_symbols = String::from(short_symbols);
     // TODO: Add a dot (explain why)
@@ -32,17 +32,17 @@ fn nr_possible_arrangements(record: &str) -> u64 {
         "Counting arrangements for symbols: {:?} counts: {:?}",
         symbols, counts
     );
-    let nr_arrangements = nr_assignments(0, &symbols, &counts, false);
+    let nr_arrangements = nr_arrangements(0, &symbols, &counts, false);
     println!("Found {} arrangements", nr_arrangements);
     nr_arrangements
 }
 
-fn nr_assignments(level: usize, symbols: &String, counts: &[u32], in_hashes: bool) -> u64 {
+fn nr_arrangements(level: usize, symbols: &String, counts: &[u32], in_hashes: bool) -> u64 {
     let is = indent_str(level);
-    // println!(
-    //     "{is}* Nr assignments for symbols: {:?} counts: {:?} in_hashes: {}",
-    //     symbols, counts, in_hashes
-    // );
+    println!(
+        "{is}* Nr assignments for symbols: {:?} counts: {:?} in_hashes: {}",
+        symbols, counts, in_hashes
+    );
     if symbols.is_empty() {
         return if counts.is_empty() {
             // println!("{is}- Symbols is empty and counts is also empty => 1 match");
@@ -75,14 +75,14 @@ fn nr_assignments(level: usize, symbols: &String, counts: &[u32], in_hashes: boo
                 // println!("{is}- Consume a hash");
                 let mut new_counts = counts.to_vec();
                 new_counts[0] -= 1;
-                nr_assignments(level + 1, &rest_symbols, &new_counts, true)
+                nr_arrangements(level + 1, &rest_symbols, &new_counts, true)
             }
         } else {
             assert!(first_count > 0);
             // println!("{is}- Starting new group of hashes, consume first hash");
             let mut new_counts = counts.to_vec();
             new_counts[0] -= 1;
-            nr_assignments(level + 1, &rest_symbols, &new_counts, true)
+            nr_arrangements(level + 1, &rest_symbols, &new_counts, true)
         }
     } else if first_symbol == '.' {
         // println!("{is}- First symbol is .");
@@ -91,21 +91,21 @@ fn nr_assignments(level: usize, symbols: &String, counts: &[u32], in_hashes: boo
             if first_count == 0 {
                 // println!("{is}- Consumed expected number of hashes");
                 let new_counts = &counts[1..];
-                nr_assignments(level + 1, &rest_symbols, new_counts, false)
+                nr_arrangements(level + 1, &rest_symbols, new_counts, false)
             } else {
                 // println!("{is}- Expected more hashes -> 0 match");
                 0
             }
         } else {
             // println!("{is}- Continuing in dots");
-            nr_assignments(level + 1, &rest_symbols, counts, false)
+            nr_arrangements(level + 1, &rest_symbols, counts, false)
         }
     } else if first_symbol == '?' {
         let rest_symbols = String::from(&symbols[1..]);
         let dash_symbol = format!("#{}", &rest_symbols);
         let dot_symbol = format!(".{}", &rest_symbols);
-        let hash_arrangements = nr_assignments(level + 1, &dash_symbol, counts, in_hashes);
-        let dot_arrangements = nr_assignments(level + 1, &dot_symbol, counts, in_hashes);
+        let hash_arrangements = nr_arrangements(level + 1, &dash_symbol, counts, in_hashes);
+        let dot_arrangements = nr_arrangements(level + 1, &dot_symbol, counts, in_hashes);
         let total_arrangements = hash_arrangements + dot_arrangements;
         // println!(
         //     "{is}- First symbol is ?, sum arrangements for # and . => {} + {} = {}",
