d80495a61fba1934d3cd44f6c514d44fe3e04bf5
Bruno Rijsman <brunorijsman@hotmail.com>
2025-10-22 17:19:28 +0200

    Start from scratch

--- day_17_a/src/main.rs
+++ day_17_a/src/main.rs
@@ -1,11 +1,3 @@
-mod dijkstra;
-mod dir;
-mod map;
-mod pos;
-
-use map::Map;
 fn main() {
-    let map = Map::from_file("example_input");
-    map.print();
-    let _cost = dijkstra::shortest_path_cost(&map);
+    println!("Hello, world!");
 }
